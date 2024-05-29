f329863459b02d37e28794ce9d2900c07dcc7edf
Tabatskiy <coldfire347@gmail.com>
2024-05-29 21:11:29 +0300

    second commit

--- src/main/java/by/dmutro/Main.java
+++ src/main/java/by/dmutro/Main.java
@@ -2,5 +2,7 @@ package by.dmutro;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Java language");
+        System.out.println("Java forever");
     }
 }
